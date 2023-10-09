12e2fe27d6fd87b4e689067751eabd3e00476c7a
Carlos Henrique <141633688+CarlosH2323@users.noreply.github.com>
2023-10-09 07:54:15 -0300

    Add files via upload

---
+++ LIMA/AULA09/script.js
@@ -0,0 +1,68 @@
+// //let - variavel local
+// //var - variavel global
+// //const - variavel constante
+
+// const obj = [
+//      {
+//     name: 'Carlos',
+//     age: 18,
+//     country: 'Brazil',
+//      },
+//      {
+//         name: 'Carlos',
+//         age: 18,
+//         country: 'Brazil',
+//     },
+//     {
+//         name: 'Carlos',
+//         age: 18,
+//         country: 'Brazil',
+//     },
+//     {
+//         name: 'Carlos',
+//         age: 18,
+//         country: 'Brazil',
+//     },
+//     {
+//         name: 'Carlos',
+//         age: 18,
+//         country: 'Brazil',
+//     },
+//     {
+//         name: 'Carlos',
+//         age: 18,
+//         country: 'Brazil',
+//     } 
+// ]
+
+// console.log(obj);
+// console.warn("AVISO");
+// console.error('ERRO');
+// console.table
+
+//operadores aritimeticos
+
+// + soma
+// - subtração
+// * multiplicação
+// / divisão
+// ** potenciação
+// % resto da divisã
+
+//Operadores de comparação
+
+// > maior que
+// < menor que
+// >= maior ou igual que 
+// <= menor ou igual que
+// = atribuição
+// == igualdade (valor)
+// != diferente (valor)
+// === igualdade (valor e tipo)
+// !== diferente (valor e tipo)
+
+var a = 10;
+var b = '10';
+
+console.log(a === b);
+
